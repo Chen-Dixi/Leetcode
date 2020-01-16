785a995205ba7fffd1540b1dd1ceb7e1813a17c8
Chen-Dixi <cdxcqu2014@163.com>
2020-01-16 18:23:32 +0800

    二叉树公共祖先，c++使用TreeNode*&引用指针在函数中修改指针内容

--- 236_lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236_lowest-common-ancestor-of-a-binary-tree.cpp
@@ -22,6 +22,31 @@ using namespace std;
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // 最近公共祖先，that is to say, 1. 节点p 和 q分别在这个祖先节点的两边。 2. 祖先节点是p,q其中之一
         
+        TreeNode* ancestor = NULL;
+        dfs(root, p, q, ancestor); //这样就可一改变它的值了
+
+        return ancestor;
+    }
+
+    // 深度遍历，返回bool标记
+    bool dfs(TreeNode* root, TreeNode* p, TreeNode* q, TreeNode*& ancestor){
+        bool right = false, left = false;
+
+        if (root==NULL){
+            return false;
+        }
+
+        left = dfs(root->left, p , q, ancestor); //在左边有找到 p 或 q
+        right = dfs(root->right, p , q, ancestor); //在右边有找到 p 或 q
+        
+        if((left && right) || ((left || right) && (root==p || root==q)))
+           ancestor = root;
+
+        if(root == p || root == q || left || right)
+            return true;
+        else
+            return false;
     }
 };
