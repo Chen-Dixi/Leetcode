8bc2714f2c6af694d5d857407a861e1388aa693c
Chen-Dixi <cdxcqu2014@163.com>
2020-01-29 20:50:26 +0800

    最小栈，偷懒直接用stack

---
+++ 155_min-stack.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <map>
+#include <utility>
+#include <algorithm>
+using namespace std;
+
+class MinStack {
+public:
+    /** initialize your data structure here. */
+    MinStack() {
+        
+    }
+    
+    void push(int x) {
+        s1.push(x);
+        
+        if (s2.empty())
+            s2.push(x);
+        else{
+            int top = s2.top();
+
+            //s2存储栈1当前最小元素
+            s2.push(top>x?x:top);
+        }
+            
+
+    }
+    
+    void pop() {
+        s2.pop();
+        s1.pop();
+    }
+    
+    int top() {
+        return s1.top();
+    }
+    
+    int getMin() {
+        return s2.top();
+    }
+
+private:
+    stack<int> s1;
+    stack<int> s2;
+};
+
+int main(){
+    MinStack* obj = new MinStack();
+    obj->push(x);
+    obj->pop();
+    int param_3 = obj->top();
+    int param_4 = obj->getMin();
+
+}
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(x);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
