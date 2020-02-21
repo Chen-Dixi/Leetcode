a908684364bc85fc2a031be1bce152f407eac1c2
Chen-Dixi <cdxcqu2014@163.com>
2020-02-21 16:22:07 +0800

    中序遍历，Morris遍历

--- 99_recover-binary-search-tree.cpp
+++ 99_recover-binary-search-tree.cpp
@@ -64,6 +64,18 @@ private:
     }
 };
 
+//Morris Traversal inorder
+/*1. 如果当前节点的左孩子为空，则输出当前节点并将其右孩子作为当前节点。
+
+2. 如果当前节点的左孩子不为空，在当前节点的左子树中找到当前节点在中序遍历下的前驱节点。
+
+   a) 如果前驱节点的右孩子为空，将它的右孩子设置为当前节点。当前节点更新为当前节点的左孩子。
+
+   b) 如果前驱节点的右孩子为当前节点，将它的右孩子重新设为空（恢复树的形状）。输出当前节点。当前节点更新为当前节点的右孩子。
+
+3. 重复以上1、2直到当前节点为空。
+*/
+
 class Solution_Morris {
 public:
     void recoverTree(TreeNode* root) {
@@ -112,11 +124,6 @@ public:
         second->val = t;
     }
 };
-
-作者：ZZYuting
-链接：https://leetcode-cn.com/problems/recover-binary-search-tree/solution/cliang-chong-fang-fa-1shi-yong-di-gui-de-zhong-xu-/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 int main(){
     return 0;
