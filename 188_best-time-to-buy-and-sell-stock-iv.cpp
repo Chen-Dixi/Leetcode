053bf1c97f05fabd4eed51128e41e67e94c9c226
Chen-Dixi <cdxcqu2014@163.com>
2020-02-23 11:37:00 +0800

    买卖股票IV，状态转移动态规划，状态压缩动态规划

--- 188_best-time-to-buy-and-sell-stock-iv.cpp
+++ 188_best-time-to-buy-and-sell-stock-iv.cpp
@@ -61,9 +61,59 @@ public:
 
 };
 
+class Solution2 {
+
+public:
+    int maxProfit(int k, vector<int>& prices) {
+        
+        int lens = prices.size();
+
+        // anomalous inputs
+        if (k == 0 || lens < 2) {
+            return 0;
+        }
+
+        if (k>=prices.size() / 2 ){
+            int res=0;
+            int diff=0;
+
+            for(int i=1;i<lens;i++){
+                diff = prices[i]-prices[i-1];
+                if (diff>0)
+                    res+=diff;
+                
+            }
+            return res;
+        }    
+
+        int dp[k][2];
+        
+        for(int j=0;j<k;j++){
+            dp[j][1]=-99999999;
+            dp[j][0]=0;
+
+        }
+
+        for(int i=0;i<lens;i++){
+            for(int j=0;j<k;j++){
+                if (j==0){
+                    dp[j][1]=max(-prices[i],dp[j][1]);
+                    
+                }else{
+                    dp[j][1]=max(dp[j][1], dp[j-1][0]-prices[i]);
+                }
+                dp[j][0]=max(dp[j][0], dp[j][1]+prices[i]);    
+            }
+        }
+        return dp[k-1][0];
+    }
+
+};
+
 int main(){
-    Solution sol = Solution();
-    vector<int> points = {1,2,4};
+    //Solution sol = Solution();
+    Solution2 sol = Solution2();
+    vector<int> points = {3,2,6,5,0,3};
     //vector<int> points = {3,2,6,5,0,3};
     int k=2;
     int ans = sol.maxProfit(k, points);
