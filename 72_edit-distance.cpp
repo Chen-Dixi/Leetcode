b54f510636870b1722eac4a74cddb6c7a5a98770
Chen-Dixi <cdxcqu2014@163.com>
2020-03-16 15:51:56 +0800

    重写编辑距离算法

--- 72_edit-distance.cpp
+++ 72_edit-distance.cpp
@@ -37,3 +37,33 @@ public:
         return dp[m][n];
     }
 };
+
+class SolutionPractice{
+public:
+    int minDistance(string word1, string word2){
+        int m = word1.size();
+        int n = word2.size();
+
+        if(m*n==0)
+            //return 0;
+            return m+n;
+
+        int dp[m+1][n+1];
+
+        for(int i=0;i<=m;i++)
+            dp[i][0]=i;
+        for(int j=0;j<=n;j++)
+            dp[0][j]=j;
+
+        for(int i=1;i<=m;i++)
+            for(int j=1;j<=n;j++)
+                if(word1[i-1]!=word2[j-1]){
+                    dp[i][j] = 1 + min( dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                }else{
+                    dp[i][j] = 1 + min( dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]-1));
+                }
+
+        return dp[m][n];
+
+    }
+};
